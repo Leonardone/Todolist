7ac0bd7303eb4b849ea773fd63f5b04fb70feaad
leo <njuccon@hotmail.it>
2017-03-13 18:15:29 +0100

    jdjkdj

--- item.controller.js
+++ item.controller.js
@@ -14,4 +14,24 @@ angular.module('app').controller('ItemController',function($scope){//creiamo il 
     $scope.elimina= function(indice){
         $scope.lista.splice(indice, 1);
     }
+
+
+    $scope.listaRicetta=[{
+        _id:"",
+        "titolo":"torta di mele",
+        "immagini":["img1", "img2"]
+
+    },{
+        _id:"",
+        "titolo":"torta di mele",
+        "immagini":["img1", "img2"]
+    }];
+
+    $scope.ricetta={
+        _id:"",
+        "titolo":"torta di mele",
+        "categoria":"torta di mele",
+        "ingredienti":["mele", "uova", "farina"],
+        "immagini":["img1", "img2"]
+    };
 });
